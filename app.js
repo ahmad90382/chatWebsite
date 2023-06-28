ffbea2ff4bb6c4165968782e8db00ffb4e68140f
ahmad90382 <137978434+ahmad90382@users.noreply.github.com>
2023-06-28 17:18:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -13,26 +14,26 @@ require("./dbConnection");
 app.use(cookieParser());
 
 // Routers
-app.use(require("./router/userRoute"));
-app.use(require("./router/contactRoute"));
-app.use(require("./router/fetchData"));
-app.use(require("./router/chatRoute"));
-app.use(require("./router/messageRoute")); // Make sure the router file is correctly required
+const userRouter = require("./router/userRoute");
+const contactRouter = require("./router/contactRoute");
+const fetchDataRouter = require("./router/fetchData");
+const chatRouter = require("./router/chatRoute");
+const messageRouter = require("./router/messageRoute");
 
-// static files
+app.use(userRouter);
+app.use(contactRouter);
+app.use(fetchDataRouter);
+app.use(chatRouter);
+app.use(messageRouter);
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
+
+// Handle all remaining routes by serving the index.html file
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.get("/", (req, res) => {
-  // Handle root route if needed
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`.bgGreen);
 });
-
-//socket
-
-module.exports = app; // Export the app object
